be1df2b6bd7fd2afa67af2e31519db5e989f0836
Myllena Cruz <myllena12345@hotmail.com>
2021-07-07 16:35:34 -0300

    Rota de envio dos formulários e capturação de dados

--- index.js
+++ index.js
@@ -4,13 +4,22 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+app.use (express.urlencoded ({ extended: true }));
+app.use (express.json());
+
 app.get("/", (req, res) => {
   res.render("index");
 });
 
 app.get("/perguntar", (req, res) => {
   res.render("perguntar");
-})
+});
+
+app.post("/saveask", (req, res) => {
+  let titulo = req.body.titulo;
+  let descricao = req.body.descricao;
+  res.send(`Recebido! Título: ${titulo} e descricação: ${descricao}`);
+});
 
 app.listen(8080, () => {
   console.log("Rodando!");
