76e73b23c5f6952d3ceb1ba11fe09e2d71a7557e
Myllena Cruz <myllena12345@hotmail.com>
2021-07-07 22:06:16 -0300

    Model de Resposta

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const connection = require("./database/database");
 const askModel = require("./database/Ask");
+const Resposta = require("./database/Resposta");
 
 connection
   .authenticate()
