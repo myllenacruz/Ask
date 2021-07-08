dc92ad9fae781c455df656aad47be54d0e93aa10
Myllena Cruz <myllena12345@hotmail.com>
2021-07-07 22:33:35 -0300

    Formulário de resposta

--- index.js
+++ index.js
@@ -62,6 +62,17 @@ app.get("/pergunta/:id", (req, res) => {
   });
 });
 
+app.post("/responder", (req, res) => {
+  let corpo = req.body.corpo;
+  let perguntaId = req.body.pergunta;
+  Resposta.create({
+    corpo: corpo,
+    perguntaId: perguntaId
+  }).then(() =>{
+    res.redirect("/pergunta/"+perguntaId);
+  });
+});
+
 app.listen(8080, () => {
   console.log("Rodando!");
 });
