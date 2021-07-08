cace83104067cf2df6f85fd0e9d3f603eb0f2926
Myllena Cruz <myllena12345@hotmail.com>
2021-07-07 22:52:49 -0300

    Exibição das respostas

--- index.js
+++ index.js
@@ -53,9 +53,19 @@ app.get("/pergunta/:id", (req, res) => {
     where: {id: id}
   }).then(pergunta => {
     if(pergunta != undefined) {
-      res.render("pergunta", {
-        pergunta: pergunta
-      });
+
+      Resposta.findAll({
+        where: {perguntaId: pergunta.id},
+        order: [ 
+          ['id', 'DESC']
+        ]
+      }).then(respostas => {
+        res.render("pergunta", {
+          pergunta: pergunta,
+          respostas: respostas
+        });
+      }); 
+      
     } else {
       res.redirect("/");
     }
